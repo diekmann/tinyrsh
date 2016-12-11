0bc8085961b9cdb6394b13a568eb1ad8305c241c
corny <none@localhost>
2016-12-11 14:12:41 +0100

    cleaning haskell

--- hs/tinyrsh/app/Main.hs
+++ hs/tinyrsh/app/Main.hs
@@ -3,14 +3,23 @@ module Main where
 import System.IO
 import Network.Socket
 import System.Posix.Process (forkProcess, executeFile)
-import System.Posix.IO (handleToFd, dupTo, stdInput, stdOutput, stdError, closeFd)
+import System.Posix.IO (handleToFd, setFdOption, dupTo, stdInput, stdOutput, stdError, closeFd)
+import qualified System.Posix.IO as FdOption (FdOption(..))
 import System.Directory (getDirectoryContents)
 import qualified System.Posix.Types (Fd)
+
+
+setSocketCloseOnExec :: Socket -> IO ()
+setSocketCloseOnExec socket =
+    setFdOption (fromIntegral $ fdSocket socket) FdOption.CloseOnExec True
+
 
 main :: IO ()
 main = do
 	putStrLn "Starting"
 	srvSock <- socket AF_INET Stream 0
+	-- Why does Haskell not support SOCK_CLOEXEC directly?
+	setSocketCloseOnExec srvSock
 	setSocketOption srvSock ReuseAddr 1
 	bind srvSock (SockAddrInet 6699 iNADDR_ANY)
 	listen srvSock 1
@@ -41,11 +50,13 @@ doChild hdl = do
 	putStrLn $ "Open fds: " ++ show dirFds2
 	--TODO properly close? what did haskell open??
 	--mapM_ closeFd [fd | fd <- dirFds2, fd < sockFd] --TODO also closes socket
-	--mapM_ closeFd $ map (read::String -> System.Posix.Types.Fd) ["0","1","2","3","4","5","6","7","8","9","10","11","12","13", "14"]
+	--mapM_ closeFd $ [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]
 	dupTo sockFd stdInput
 	dupTo sockFd stdOutput
 	dupTo sockFd stdError
-	executeFile "/bin/sh" False [] Nothing
+	-- Somethime we get "tinyrsh-exe: failed to create OS thread: Resource temporarily unavailable"
+	-- But the shell spawns up. What is Haskell trying to do?
+	executeFile "/home/corny/git/tinyrsh/exectest/exectest" False [] Nothing --"/bin/sh" False [] Nothing
 	putStrLn "unreachable"
 	
 
