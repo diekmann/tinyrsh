3a291e6b811701f24e19d979ef26bb6051655196
corny <none@localhost>
2016-12-21 21:46:19 +0100

    fd cleanup

--- hs-idiomatic/tinyrsh/app/Main.hs
+++ hs-idiomatic/tinyrsh/app/Main.hs
@@ -3,14 +3,12 @@ module Main where
 import System.IO
 import Network.Socket
 import qualified System.Process as SysProc
-import qualified Lib
 
 
 main :: IO ()
 main = do
     putStrLn "Starting"
     srvSock <- socket AF_INET Stream 0
-    Lib.setSocketCloseOnExec srvSock
     setSocketOption srvSock ReuseAddr 1
     bind srvSock (SockAddrInet 6699 iNADDR_ANY)
     listen srvSock 1
@@ -27,10 +25,11 @@ sockLoop srvSock = do
 handleClient :: Handle -> IO ()
 handleClient hdl = do
     putStrLn $ "handle client"
-    let shProcess = (SysProc.proc "/bin/sh" []){ SysProc.std_out = SysProc.UseHandle hdl,
-                                                 SysProc.std_in = SysProc.UseHandle hdl,
-                                                 SysProc.std_err = SysProc.UseHandle hdl }
-    (cin, cout, cerr, ph) <- SysProc.createProcess shProcess
-    putStrLn $ "created Process " ++ show cin
+    let shProcess = (SysProc.proc "/bin/sh" []){ SysProc.std_in = SysProc.UseHandle hdl,
+                                                 SysProc.std_out = SysProc.UseHandle hdl,
+                                                 SysProc.std_err = SysProc.UseHandle hdl,
+                                                 SysProc.close_fds = True }
+    (Nothing, Nothing, Nothing, ph) <- SysProc.createProcess shProcess
+    putStrLn $ "created Process"
     --TODO: reap zombies, make interactive python spawning possible.
 
--- hs-idiomatic/tinyrsh/src/Lib.hs
+++ hs-idiomatic/tinyrsh/src/Lib.hs
@@ -1,14 +1,4 @@
 module Lib
-    ( setSocketCloseOnExec
+    (
     ) where
-
-import Network.Socket
-import System.Posix.IO (setFdOption)
-import qualified System.Posix.IO as FdOption (FdOption(..))
-
-
--- Why does Haskell not support SOCK_CLOEXEC directly?
-setSocketCloseOnExec :: Socket -> IO ()
-setSocketCloseOnExec socket =
-    setFdOption (fromIntegral $ fdSocket socket) FdOption.CloseOnExec True
 
