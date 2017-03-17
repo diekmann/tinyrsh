56918048b41216eaffa8e90176395f388e0c757b
diekmann <diekmann@net.in.tum.de>
2017-03-17 15:48:23 +0100

    Revert "[super broken] spawn shell first, let clients reattach"
    This reverts commit d26ea3ded96daf86f0e11527995e752f7e42d1dd.

--- py-ptysh/ptysh.py
+++ py-ptysh/ptysh.py
@@ -2,19 +2,12 @@
 import sys
 import pty
 
-print("ptysh", file=sys.stderr, flush=True)
-
-print("hello", flush=True)
-
-x = input()
-print("you said {}".format(x), flush=True)
-
 ret = pty.spawn('/bin/sh')
 
 assert ret < 2**16
 killsig = ret & 0xff
 if killsig != 0:
-    print("abnormal termination", file=sys.stderr)
+    print("abnormal termination")
 retcode = (ret & 0xff00) >> 8
 
 print("exited {}".format(retcode), file=sys.stderr)
